4f78573b072c28dfc3266e09cdcef99b35394b9c
vaibhav <80770886+VaibhavGeete@users.noreply.github.com>
2021-09-09 20:23:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+function preload()
+{
+
+}
+
+function draw()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(300,300);
+    canvas.center();
+}
+
+function check()
+{
+    save('vaibhav.png');
+    
+}
